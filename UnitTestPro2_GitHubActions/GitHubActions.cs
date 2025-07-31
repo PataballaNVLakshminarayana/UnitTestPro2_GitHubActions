9af976e798181333c473be6f7ae485e1496a3937
Lenovo <Lenovo@LAKSHMINARAYANA>
2025-07-31 17:25:21 +0530

    Add project files.

---
+++ UnitTestPro2_GitHubActions/GitHubActions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace UnitTestPro2_GitHubActions
+{
+    [TestClass]
+    public class GitHubActions
+    {
+        public static IWebDriver _driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://adactinhotelapp.com/");
+            _driver.FindElement(By.Id("username")).SendKeys("nagalakshmin");
+            _driver.FindElement(By.Id("password")).SendKeys("l@kshmin");
+            _driver.FindElement(By.Id("login")).Submit();
+            Thread.Sleep(10000);
+            _driver.Quit();
+        }
+    }
+}
---
+++ UnitTestPro2_GitHubActions/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPro2_GitHubActions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPro2_GitHubActions")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb960fad-50a6-42f5-9994-163254cb2361")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
